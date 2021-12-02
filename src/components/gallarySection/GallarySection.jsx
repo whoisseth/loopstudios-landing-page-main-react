b35d498eda653795cb22d3d41910796abfc29c16
utkarsh seth <utkarshseth2019@gmail.com>
2021-12-02 22:23:51 +0530

    gallry through object  Section Complete

--- src/components/gallarySection/GallarySection.jsx
+++ src/components/gallarySection/GallarySection.jsx
@@ -1,45 +1,58 @@
-import DeepEarthImg from "./../../images/desktop/image-deep-earth.jpg";
 import "./GallarySection.scss";
+
 function GallarySection() {
-    // const GallarObj ={
-    //     ImageAddress:"./../../images/desktop/image-deep-earth.jpg"
-    //     ImageName:"DEEP EARTH"
-    // }
+  const gallaryObj = [
+    {
+      id: 1,
+      ImageAddress: "/images/desktop/image-deep-earth.jpg",
+      ImageName: "DEEP EARTH",
+    },
+    {
+      id: 2,
+      ImageAddress: "/images/desktop/image-night-arcade.jpg",
+      ImageName: "NIGHT ARCADE",
+    },
+    {
+      id: 3,
+      ImageAddress: "/images/desktop/image-soccer-team.jpg",
+      ImageName: "SOCCER TEAM VR",
+    },
+    {
+      id: 4,
+      ImageAddress: "/images/desktop/image-grid.jpg",
+      ImageName: " THE GRID",
+    },
+    {
+      id: 5,
+      ImageAddress: "/images/desktop/image-from-above.jpg",
+      ImageName: "FROM UP ABOVE VR ",
+    },
+    {
+      id: 6,
+      ImageAddress: "/images/desktop/image-pocket-borealis.jpg",
+      ImageName: "POCKET BOREALIS",
+    },
+    {
+      id: 7,
+      ImageAddress: "/images/desktop/image-curiosity.jpg",
+      ImageName: "THE CURIOSITY ",
+    },
+    {
+      id: 8,
+      ImageAddress: "/images/desktop/image-fisheye.jpg",
+      ImageName: "MAKE IT FISHEYE ",
+    },
+  ];
+
   return (
     <div>
       <div className="GallarySection">
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
-        <div className="cart">
-          <img src={DeepEarthImg} alt="DeepEarthImg" />
-          <div className="title">DEEP EARTH</div>
-        </div>
+        {gallaryObj.map((gallary) => (
+          <div className="cart" key={gallaryObj.id}>
+            <img src={gallary.ImageAddress} alt="" />
+            <div className="title">{gallary.ImageName}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
